9ada32aa85e94a9ad89ca0408448a0dbbf2a5acf
Rolorunner <alex.denys.lovett@gmail.com>
2018-06-25 16:27:23 +0100

    added grabbing script

---
+++ HandPicker/HandPicker/Assets/ObjectMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour {
+    private GameObject chosenObject;
+    bool grabbing;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (grabbing)
+        {
+            chosenObject.transform.position = transform.position;
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        chosenObject = collision.gameObject;
+    }
+
+    void Grab()
+    {
+        grabbing = !grabbing;
+    }
+}
