5ef24dcea35b17ec830bf2e87b44e92104a6a055
Adam Tunnicliffe <adam.j.tunnicliffe@gmail.com>
2018-06-25 23:19:06 +0100

    improved script quality

--- HandPicker/HandPicker/Assets/PlayerController.cs
+++ HandPicker/HandPicker/Assets/PlayerController.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    //public float speed;
-
+    float SPEED = 10;
+    Quaternion tempRotate;
     private Rigidbody rb;
 
     void Start()
@@ -15,36 +15,37 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (Input.GetAxis("Horizontal") < 0 || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetAxis("Horizontal") < 0 )
         {
             Debug.Log("Leftarrow detected!");
-            transform.position += Vector3.right * Time.deltaTime * 10;
+            transform.position += Vector3.left * Time.deltaTime * SPEED;
+        
         }
 
         if (Input.GetAxis("Horizontal") > 0 )
         {
             Debug.Log("Rightarrow detected!");
-            transform.position += Vector3.left * Time.deltaTime * 10;
+            transform.position += Vector3.right * Time.deltaTime * SPEED;
         }
         if (Input.GetAxis("Vertical") > 0 )
         {
             Debug.Log("Rightarrow detected!");
-            transform.position += Vector3.forward * Time.deltaTime * 10;
+            transform.position += Vector3.forward * Time.deltaTime * SPEED;
         }
         if (Input.GetAxis("Vertical") < 0 )
         {
             Debug.Log("Rightarrow detected!");
-            transform.position += Vector3.back * Time.deltaTime * 10;
+            transform.position += Vector3.back * Time.deltaTime * SPEED;
         }
         if (Input.GetKey(KeyCode.Joystick1Button4))
         {
             Debug.Log("Leftarrow detected!");
-            transform.position += Vector3.up * Time.deltaTime * 10;
+            transform.position += Vector3.up * Time.deltaTime * SPEED;
         }
          if (Input.GetKey(KeyCode.Joystick1Button5))
         {
             Debug.Log("Leftarrow detected!");
-            transform.position += Vector3.down * Time.deltaTime * 10;
+            transform.position += Vector3.down * Time.deltaTime * SPEED;
         }
     }
 }
--- HandPicker/HandPicker/Assets/ThirdPersonCam.cs
+++ HandPicker/HandPicker/Assets/ThirdPersonCam.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 public class ThirdPersonCam : MonoBehaviour {
 
     public GameObject hand;
-    public Vector3 offset = new Vector3(5,-5,5);
+    public Vector3 offset = new Vector3(0,-5,5);
     
 	void Start () {
-
-       // offset = hand.transform.position - transform.position;
+        
     }
 	
 	void LateUpdate () {
 
         //Get Y Rotation of hand
-        Quaternion rotation = Quaternion.Euler(0, hand.transform.eulerAngles.y, 0);
+        Quaternion handRotation = Quaternion.Euler(0, hand.transform.eulerAngles.y, 0);
 
         //Get Position of hand
         Vector3 handPos = hand.transform.position;
 
         //Apply both to Camera
-        transform.position = handPos - (rotation * offset);
+        transform.position = handPos - (handRotation * offset);
 
         //Look at Camera
         transform.LookAt(hand.transform);
