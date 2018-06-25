0a1f9d8794e134fc942beda1c9010b4017283a42
Alex Lovett <alex.denys.lovett@gmail.com>
2018-06-25 21:49:59 +0100

    cleaned code

--- HandPicker/HandPicker/Assets/ObjectMovement.cs
+++ HandPicker/HandPicker/Assets/ObjectMovement.cs
@@ -4,40 +4,81 @@ using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour
 {
-    public float RotateAmount;
     private GameObject chosenObject;
     bool grabbing = false;
+    public KeyCode Enlarge, Shrink, RotateL, RotateR, Move;
+    public float RotateSpeed, ScaleSpeed;
+    Quaternion tempRotate;
     // Use this for initialization
     void Start()
     {
 
     }
 
+    private void FixedUpdate()
+    {
+        //if (grabbing)
+        //{
+        //    chosenObject.transform.position = transform.position;
 
+        //    if (Input.GetKey(Enlarge))
+        //    {
+        //        chosenObject.transform.localScale += new Vector3(ScaleSpeed, ScaleSpeed, ScaleSpeed);
+        //    }
+        //    if (Input.GetKey(Shrink))
+        //    {
+        //        chosenObject.transform.localScale -= new Vector3(ScaleSpeed, ScaleSpeed, ScaleSpeed);
+        //    }
+        //    if (Input.GetKey(RotateL))
+        //    {
+        //        tempRotate = chosenObject.transform.rotation;
+        //        tempRotate.y += RotateSpeed;
+        //        chosenObject.transform.rotation = tempRotate;
+        //    }
+        //    if (Input.GetKey(RotateR))
+        //    {
+        //        tempRotate = chosenObject.transform.rotation;
+        //        tempRotate.y -= RotateSpeed;
+        //        chosenObject.transform.rotation = tempRotate;
+        //    }
+        //}
+    }
 
     private void OnTriggerStay(Collider collision)
     {
         chosenObject = collision.gameObject;
-        if (Input.GetKeyUp(KeyCode.Joystick1Button0))
+        if (Input.GetKeyUp(Move))
         {
-            Debug.Log("pressed");
             Grab();
         }
         if (grabbing)
         {
             chosenObject.transform.position = transform.position;
-            if (Input.GetKey(KeyCode.Joystick1Button2))
+
+            if (Input.GetKey(Enlarge))
             {
-                chosenObject.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+                chosenObject.transform.localScale += new Vector3(ScaleSpeed, ScaleSpeed, ScaleSpeed);
             }
-            if (Input.GetKey(KeyCode.Joystick1Button3))
+            if (Input.GetKey(Shrink))
             {
-                chosenObject.transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+                chosenObject.transform.localScale -= new Vector3(ScaleSpeed, ScaleSpeed, ScaleSpeed);
+            }
+            if (Input.GetKey(RotateL))
+            {
+                tempRotate = chosenObject.transform.rotation;
+                tempRotate.y += RotateSpeed;
+                chosenObject.transform.rotation = tempRotate;
+            }
+            if (Input.GetKey(RotateR))
+            {
+                tempRotate = chosenObject.transform.rotation;
+                tempRotate.y -= RotateSpeed;
+                chosenObject.transform.rotation = tempRotate;
             }
         }
     }
 
-    void Grab()
+    void Grab() //toggles if the hand is grabbing an object
     {
         grabbing = !grabbing;
     }
