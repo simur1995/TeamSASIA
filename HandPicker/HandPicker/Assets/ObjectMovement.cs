a717f1130ff33b59920ada10ebd8cebbb55a09ee
Rolorunner <alex.denys.lovett@gmail.com>
2018-06-25 16:54:53 +0100

    updated object movemnt

--- HandPicker/HandPicker/Assets/ObjectMovement.cs
+++ HandPicker/HandPicker/Assets/ObjectMovement.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour {
+    public float RotateAmount;
     private GameObject chosenObject;
     bool grabbing;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButton("Joystick 1 Button 0"))
+        {
+            Grab();
+        }
         if (grabbing)
         {
             chosenObject.transform.position = transform.position;
         }
-	}
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -27,4 +32,25 @@ public class ObjectMovement : MonoBehaviour {
     {
         grabbing = !grabbing;
     }
+
+    //void Rotate(bool rotate)
+    //{
+    //    chosenObject =
+    //}
+
+    //void ButtonMenu(string selection)
+    //{
+    //    switch (selection)
+    //    {
+    //        case "Joystick 1 Button 7":
+    //            Rotate(false);
+    //            break;
+    //        case "Joystick 1 Button 8":
+    //            Rotate(true);
+    //            break;
+    //        default:
+    //            break;
+    //    }
+    //}
 }
+
