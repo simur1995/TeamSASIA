55eeacd13ddd02253922909dccedd7092d4105eb
Adam Tunnicliffe <adam.j.tunnicliffe@gmail.com>
2018-06-25 23:58:22 +0100

    Fixed script bugs, changed controls
    Added code to make controls different. old control code still there ust commented out.

--- HandPicker/HandPicker/Assets/PlayerController.cs
+++ HandPicker/HandPicker/Assets/PlayerController.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    float SPEED = 10;
+    float MOVESPEED = 5;
+    float ROTSPEED = 60;
     Quaternion tempRotate;
     private Rigidbody rb;
 
@@ -18,34 +19,45 @@ public class PlayerController : MonoBehaviour
         if (Input.GetAxis("Horizontal") < 0 )
         {
             Debug.Log("Leftarrow detected!");
-            transform.position += Vector3.left * Time.deltaTime * SPEED;
-        
-        }
 
+            //transform.position += Vector3.left * Time.deltaTime * SPEED;
+
+            transform.Rotate(new Vector3(0, Time.deltaTime * -ROTSPEED, 0));
+        }
         if (Input.GetAxis("Horizontal") > 0 )
         {
             Debug.Log("Rightarrow detected!");
-            transform.position += Vector3.right * Time.deltaTime * SPEED;
+
+            //transform.position += Vector3.right * Time.deltaTime * SPEED;
+
+            transform.Rotate(new Vector3(0, Time.deltaTime * ROTSPEED, 0));
         }
+
         if (Input.GetAxis("Vertical") > 0 )
         {
             Debug.Log("Rightarrow detected!");
-            transform.position += Vector3.forward * Time.deltaTime * SPEED;
+            //transform.position += Vector3.forward * Time.deltaTime * SPEED;
+
+            transform.Translate(Time.deltaTime * MOVESPEED * Vector3.forward);
         }
         if (Input.GetAxis("Vertical") < 0 )
         {
             Debug.Log("Rightarrow detected!");
-            transform.position += Vector3.back * Time.deltaTime * SPEED;
+            //transform.position += Vector3.back * Time.deltaTime * SPEED;
+
+            transform.Translate(Time.deltaTime * MOVESPEED *Vector3.back);
         }
+
         if (Input.GetKey(KeyCode.Joystick1Button4))
         {
             Debug.Log("Leftarrow detected!");
-            transform.position += Vector3.up * Time.deltaTime * SPEED;
+            transform.position += Vector3.up * Time.deltaTime * MOVESPEED;
         }
+
          if (Input.GetKey(KeyCode.Joystick1Button5))
         {
             Debug.Log("Leftarrow detected!");
-            transform.position += Vector3.down * Time.deltaTime * SPEED;
+            transform.position += Vector3.down * Time.deltaTime * MOVESPEED;
         }
     }
 }
