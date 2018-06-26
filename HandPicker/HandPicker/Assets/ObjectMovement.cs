0dc3b0e5d130e7890eb970abc62a1316428925de
Alex Lovett <alex.denys.lovett@gmail.com>
2018-06-26 08:52:02 +0100

    cleaned up code

--- HandPicker/HandPicker/Assets/ObjectMovement.cs
+++ HandPicker/HandPicker/Assets/ObjectMovement.cs
@@ -6,7 +6,7 @@ public class ObjectMovement : MonoBehaviour
 {
     private GameObject chosenObject;
     bool grabbing = false;
-    public KeyCode Enlarge, Shrink, RotateL, RotateR, Move;
+    public KeyCode Enlarge, Shrink, RotateL, RotateR, MoveObject;
     public float RotateSpeed, ScaleSpeed;
     Quaternion tempRotate;
     // Use this for initialization
@@ -17,40 +17,6 @@ public class ObjectMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //if (grabbing)
-        //{
-        //    chosenObject.transform.position = transform.position;
-
-        //    if (Input.GetKey(Enlarge))
-        //    {
-        //        chosenObject.transform.localScale += new Vector3(ScaleSpeed, ScaleSpeed, ScaleSpeed);
-        //    }
-        //    if (Input.GetKey(Shrink))
-        //    {
-        //        chosenObject.transform.localScale -= new Vector3(ScaleSpeed, ScaleSpeed, ScaleSpeed);
-        //    }
-        //    if (Input.GetKey(RotateL))
-        //    {
-        //        tempRotate = chosenObject.transform.rotation;
-        //        tempRotate.y += RotateSpeed;
-        //        chosenObject.transform.rotation = tempRotate;
-        //    }
-        //    if (Input.GetKey(RotateR))
-        //    {
-        //        tempRotate = chosenObject.transform.rotation;
-        //        tempRotate.y -= RotateSpeed;
-        //        chosenObject.transform.rotation = tempRotate;
-        //    }
-        //}
-    }
-
-    private void OnTriggerStay(Collider collision)
-    {
-        chosenObject = collision.gameObject;
-        if (Input.GetKeyUp(Move))
-        {
-            Grab();
-        }
         if (grabbing)
         {
             chosenObject.transform.position = transform.position;
@@ -75,6 +41,15 @@ public class ObjectMovement : MonoBehaviour
                 tempRotate.y -= RotateSpeed;
                 chosenObject.transform.rotation = tempRotate;
             }
+        }
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        chosenObject = collision.gameObject;
+        if (Input.GetKeyUp(MoveObject))
+        {
+            Grab();
         }
     }
 
