6c9b07f181e100c56394693233dd456c24b9b24b
Sijan Shreesh Rana <Sijan_Rana@hotmail.com>
2018-06-26 14:33:04 +0100

    added more useless scripts

---
+++ HandPicker/HandPicker/Assets/Scripts/FindVERTXobj.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindVERTXobj : MonoBehaviour {
+
+    public GameObject collidingObject;
+    public Vector3 objectPosition;
+
+    private void OnTriggerEnter(Collision collision)
+    {
+        collidingObject = collision.gameObject;
+
+        if(collidingObject.tag != "Floor")
+        {
+            collidingObject.AddComponent<Rigidbody>();
+            collidingObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
+
+        
+    }
+
+
+}
