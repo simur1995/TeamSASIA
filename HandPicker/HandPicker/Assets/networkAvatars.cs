aa0160c74cda0c5e3e0a3706ee7b1b4ece1cbc95
Adam Tunnicliffe <adam.j.tunnicliffe@gmail.com>
2018-06-27 14:50:48 +0100

    NetworkAvatar

---
+++ HandPicker/HandPicker/Assets/networkAvatars.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class networkAvatars : MonoBehaviour {
+
+    GameObject viewpoints;
+    public GameObject avatar;
+    int numberOfPlayers = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        viewpoints = transform.Find("Viewpoints").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (numberOfPlayers != viewpoints.transform.childCount)
+        {
+            numberOfPlayers = viewpoints.transform.childCount;
+            var user = Instantiate(avatar);
+            user.transform.parent = viewpoints.transform.GetChild(numberOfPlayers - 1);
+            user.transform.localPosition = Vector3.zero;
+        }
+		
+	}
+}
