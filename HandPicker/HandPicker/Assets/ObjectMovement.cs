daef2672c01f5e4f25fec4571704d4a648601c03
Adam Tunnicliffe <adam.j.tunnicliffe@gmail.com>
2018-06-27 10:05:18 +0100

    z rotation

--- HandPicker/HandPicker/Assets/ObjectMovement.cs
+++ HandPicker/HandPicker/Assets/ObjectMovement.cs
@@ -62,13 +62,18 @@ public class ObjectMovement : MonoBehaviour
 
             if (Input.GetAxis("Left Trigger") > 0)
             {
+                if (Input.GetKey(KeyCode.Joystick1Button2))
+                {
+                    chosenObject.transform.Rotate(new Vector3(0, 0, -SnapDegrees));
+                }
+                else
                 if (Input.GetKey(Rotate))
                 {
                     chosenObject.transform.Rotate(new Vector3(0, -SnapDegrees, 0));
                 }
                 else
                 {
-                    if (snapBool && !(Input.GetKey(Scale)))
+                    if (snapBool && !(Input.GetKey(Scale)) && !(Input.GetKey(KeyCode.Joystick1Button2)))
                     {
                         chosenObject.transform.Rotate(new Vector3(0, 45, 0));
                         snapBool = false;
@@ -78,13 +83,17 @@ public class ObjectMovement : MonoBehaviour
             }
             if (Input.GetAxis("Right Trigger") > 0)
             {
+                if (Input.GetKey(KeyCode.Joystick1Button2))
+                {
+                    chosenObject.transform.Rotate(new Vector3(0, 0, SnapDegrees));
+                }
                 if (Input.GetKey(Rotate))
                 {
                     chosenObject.transform.Rotate(new Vector3(0, SnapDegrees, 0));
                 }
                 else
                 {
-                    if (snapBool && !(Input.GetKey(Scale)))
+                    if (snapBool && !(Input.GetKey(Scale)) && !(Input.GetKey(KeyCode.Joystick1Button2)))
                     {
                         chosenObject.transform.Rotate(new Vector3(0, -45, 0));
                         snapBool = false;
