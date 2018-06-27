bea0e68cdf3c949dff8de57dfb69fa05ff1e7682
Sijan Shreesh Rana <Sijan_Rana@hotmail.com>
2018-06-27 16:56:04 +0100

    added rigidbody script

---
+++ HandPicker/HandPicker/Assets/Scripts/PlayerCountText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VertexUnityPlayer;
+
+public class PlayerCountText : MonoBehaviour {
+
+    public int playerCount;
+    private Text playerCountText;
+
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log("asaf  "+ NodeLink.CurrentNodes());
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+        //Text textObject = gameObject.GetComponent<Text>(); 
+        //textObject.text = "PLAYER COUNT: " + playerCount;
+
+
+    }
+
+    void CountObjects()
+    {
+        NodeLink.CurrentNodes();
+    }
+}
